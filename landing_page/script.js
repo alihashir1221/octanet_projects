a51de41bd530f62c54efd69113444cc3bcbcb9a9
Ali Hashir <114869206+alihashir1221@users.noreply.github.com>
2024-07-04 00:32:51 +0530

    Update script.js
    removed unnecessary elements

--- landing_page/script.js
+++ landing_page/script.js
@@ -6,23 +6,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     btn.addEventListener('mouseover', () => {
         heroBg.style.backgroundImage = "url('img/bg-light.jpg')";
     });
-
     btn.addEventListener('mouseout', () => {
         heroBg.style.backgroundImage = "url('img/bg.jpg')";
-    });
-
-    // Add scroll event to animate content on scroll
-    window.addEventListener('scroll', () => {
-        const scrollPos = window.scrollY + window.innerHeight;
-        contentSections.forEach(section => {
-            const sectionPos = section.getBoundingClientRect().top + window.scrollY;
-            if (scrollPos > sectionPos) {
-                section.classList.add('animate-front');
-                section.classList.remove('fade-out');
-            } else {
-                section.classList.remove('animate-front');
-                section.classList.add('fade-out');
-            }
-        });
     });
 });
